440b5c7239024a3d2f6ab9a67ef229ced547c2ae
cass1618 <cass16180@gmail.com>
2021-06-27 22:10:27 -0700

    :card_file_box: Move files into component directory.

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+    const user1 = "Jason";
+    const user2 = "Elanor"
+
+    return (
+        <React.Fragment>
+            <h1>Help Queue</h1>
+            <h3>3a</h3>
+            <h3>{user1} and {user2}</h3>
+            <p><em>We don't belong here!!</em></p>
+            <hr/>
+        </React.Fragment>
+    );
+}
+
+export default App;
