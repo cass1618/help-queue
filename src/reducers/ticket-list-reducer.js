082b4444e72b02a4a0f112d1d1e1a713fec84327
cass1618 <cass16180@gmail.com>
2021-07-06 21:17:10 -0700

    :white_check_mark: Pass test for add ticket

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,5 +1,17 @@
-
-
 export default (state = {}, action) => {
-    return state;
+    const {user1, user2, location, issue, id} = action;
+    switch(action.type) {
+        case "ADD_TICKET":
+            return Object.assign({}, state, {
+                [id]: {
+                    user1: user1,
+                    user2: user2,
+                    location: location,
+                    issue: issue,
+                    id: id
+                }
+            });
+            default: 
+                return state;
+    }
 };
