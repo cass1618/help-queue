6018677bfbaa9942302987169231862c8996fdf6
cass1618 <cass16180@gmail.com>
2021-07-06 21:10:48 -0700

    :test_tube: Add test for add new ticket reducer, good fail

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,8 +1,38 @@
 import ticketListReducer from "../../reducers/ticket-list-reducer";
-
 describe("ticketListReducer", () => {
+
+    let action;
+    const ticketData = {
+        user1: "Elanor",
+        user2: "Jason",
+        location: "The Good Place",
+        issue: "We don't belong here!",
+        id: 1
+    };
     test("Should return default state if there is no action type passed into the reducer", () => {
         expect(ticketListReducer({}, {type: null})).toEqual({});
     });
     //expect(current state, action to be applied - action's type is stored inside object)
+
+    test("Should successfully add new ticket data to masterTicketList", () => {
+        const {user1, user2, location, issue, id} = ticketData;
+        action = {
+            type: "ADD_TICKET",
+            user1: user1,
+            user2: user2,
+            location: location,
+            issue: issue,
+            id: id
+        }
+
+        expect(ticketListReducer({}, action)).toEqual({
+            [id] : {
+                user1: user1,
+                user2: user2,
+                location: location,
+                issue: issue,
+                id: id
+            }
+        });
+    });
 });
