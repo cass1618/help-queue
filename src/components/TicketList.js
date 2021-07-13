36b2ab49fb2fb85cdcac6eabf79df0e6a4f05d66
cass1618 <cass16180@gmail.com>
2021-07-12 20:31:24 -0700

    :card_file_box: Set up firebase to store data

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,29 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ticket from "./Ticket";
+import {useSelector} from "react-redux";
+import {useFirestoreConnect, isLoaded, isEmpty, useFirestore} from "react-redux-firebase";
 
 function TicketList(props) {
-    return (
-        <React.Fragment>
-        <hr/>
-        {Object.values(props.ticketList).map((ticket) => 
-            <Ticket
-            whenTicketClicked={props.onTicketSelection}
-            user1 = {ticket.user1}
-            user2 = {ticket.user2}
-            location={ticket.location}
-            issue={ticket.issue}
-            formattedWaitTime={ticket.formattedWaitTime}
-            id={ticket.id}
-            key={ticket.id}/>
-            
-        )}
+
+    useFirestoreConnect([
+        {collection: "tickets"}
+    ]);
+
+    const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+    if(isLoaded(tickets)) {
+        return (
+            <React.Fragment>
+            <hr/>
+                {tickets.map((ticket) => {
+
+                    return <Ticket
+                    whenTicketClicked={props.onTicketSelection}
+                    user1 = {ticket.user1}
+                    user2 = {ticket.user2}
+                    location={ticket.location}
+                    issue={ticket.issue}
+                    formattedWaitTime={ticket.formattedWaitTime}
+                    id={ticket.id}
+                    key={ticket.id}/>
+                    
+                })}
         </React.Fragment>
-    );
+        )
+    } else {
+        return(
+            <React.Fragment>
+                <h3>Loading...</h3>
+            </React.Fragment>
+        )
+    }
 }
 
 TicketList.propTypes = {
-    ticketList: PropTypes.object,
+    // ticketList: PropTypes.object,
     onTicketSelection: PropTypes.func
 };
 
